a2cb413b07a8620a4e48f816544eef13c939293c
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2022-10-10 00:25:31 +0700

    u

--- src/ORSAPR/ORSAPR.View/MainForm.Designer.cs
+++ src/ORSAPR/ORSAPR.View/MainForm.Designer.cs
@@ -29,31 +29,34 @@ namespace ORSAPR.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.LabelWidht = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.LabelWidth = new System.Windows.Forms.Label();
             this.LabelLenght = new System.Windows.Forms.Label();
             this.LabelHeight = new System.Windows.Forms.Label();
             this.LabelInnerLenght = new System.Windows.Forms.Label();
             this.LabelBladeLenght = new System.Windows.Forms.Label();
             this.TextBoxHeight = new System.Windows.Forms.TextBox();
             this.TextBoxLenght = new System.Windows.Forms.TextBox();
-            this.TextBoxWidht = new System.Windows.Forms.TextBox();
+            this.TextBoxWidth = new System.Windows.Forms.TextBox();
             this.TextBoxBladeLenght = new System.Windows.Forms.TextBox();
             this.TextBoxInnerLenght = new System.Windows.Forms.TextBox();
             this.ButtonSave = new System.Windows.Forms.Button();
             this.PanelWindow = new System.Windows.Forms.Panel();
             this.LabelParameter = new System.Windows.Forms.Label();
+            this.toolTipInformation = new System.Windows.Forms.ToolTip(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.PanelWindow.SuspendLayout();
             this.SuspendLayout();
             // 
-            // LabelWidht
+            // LabelWidth
             // 
-            this.LabelWidht.AutoSize = true;
-            this.LabelWidht.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.LabelWidht.Location = new System.Drawing.Point(25, 201);
-            this.LabelWidht.Name = "LabelWidht";
-            this.LabelWidht.Size = new System.Drawing.Size(142, 18);
-            this.LabelWidht.TabIndex = 1;
-            this.LabelWidht.Text = "W - Ширина зубила";
+            this.LabelWidth.AutoSize = true;
+            this.LabelWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.LabelWidth.Location = new System.Drawing.Point(25, 201);
+            this.LabelWidth.Name = "LabelWidth";
+            this.LabelWidth.Size = new System.Drawing.Size(142, 18);
+            this.LabelWidth.TabIndex = 1;
+            this.LabelWidth.Text = "W - Ширина зубила";
             // 
             // LabelLenght
             // 
@@ -113,14 +116,15 @@ namespace ORSAPR.View
             this.TextBoxLenght.Size = new System.Drawing.Size(213, 22);
             this.TextBoxLenght.TabIndex = 7;
             // 
-            // TextBoxWidht
+            // TextBoxWidth
             // 
-            this.TextBoxWidht.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.TextBoxWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TextBoxWidht.Location = new System.Drawing.Point(236, 200);
-            this.TextBoxWidht.Name = "TextBoxWidht";
-            this.TextBoxWidht.Size = new System.Drawing.Size(213, 22);
-            this.TextBoxWidht.TabIndex = 8;
+            this.TextBoxWidth.Location = new System.Drawing.Point(236, 200);
+            this.TextBoxWidth.Name = "TextBoxWidth";
+            this.TextBoxWidth.Size = new System.Drawing.Size(213, 22);
+            this.TextBoxWidth.TabIndex = 8;
+            this.TextBoxWidth.TextChanged += new System.EventHandler(this.TextBoxWidth_TextChanged);
             // 
             // TextBoxBladeLenght
             // 
@@ -157,18 +161,19 @@ namespace ORSAPR.View
             this.PanelWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.PanelWindow.Controls.Add(this.toolStrip1);
             this.PanelWindow.Controls.Add(this.LabelParameter);
             this.PanelWindow.Controls.Add(this.TextBoxLenght);
             this.PanelWindow.Controls.Add(this.ButtonSave);
             this.PanelWindow.Controls.Add(this.TextBoxHeight);
             this.PanelWindow.Controls.Add(this.TextBoxInnerLenght);
-            this.PanelWindow.Controls.Add(this.TextBoxWidht);
+            this.PanelWindow.Controls.Add(this.TextBoxWidth);
             this.PanelWindow.Controls.Add(this.LabelBladeLenght);
             this.PanelWindow.Controls.Add(this.TextBoxBladeLenght);
             this.PanelWindow.Controls.Add(this.LabelInnerLenght);
             this.PanelWindow.Controls.Add(this.LabelHeight);
             this.PanelWindow.Controls.Add(this.LabelLenght);
-            this.PanelWindow.Controls.Add(this.LabelWidht);
+            this.PanelWindow.Controls.Add(this.LabelWidth);
             this.PanelWindow.Location = new System.Drawing.Point(12, 12);
             this.PanelWindow.MaximumSize = new System.Drawing.Size(920, 1160);
             this.PanelWindow.MinimumSize = new System.Drawing.Size(450, 520);
@@ -187,6 +192,15 @@ namespace ORSAPR.View
             this.LabelParameter.TabIndex = 12;
             this.LabelParameter.Text = "Параметры инструмента";
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(460, 25);
+            this.toolStrip1.TabIndex = 13;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
             // FormParameters
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -197,6 +211,8 @@ namespace ORSAPR.View
             this.MinimumSize = new System.Drawing.Size(491, 591);
             this.Name = "FormParameters";
             this.Text = "Настройки";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormParameters_FormClosing);
+            this.Load += new System.EventHandler(this.FormParameters_Load);
             this.PanelWindow.ResumeLayout(false);
             this.PanelWindow.PerformLayout();
             this.ResumeLayout(false);
@@ -205,19 +221,21 @@ namespace ORSAPR.View
 
         #endregion
 
-        private System.Windows.Forms.Label LabelWidht;
+        private System.Windows.Forms.Label LabelWidth;
         private System.Windows.Forms.Label LabelLenght;
         private System.Windows.Forms.Label LabelHeight;
         private System.Windows.Forms.Label LabelInnerLenght;
         private System.Windows.Forms.Label LabelBladeLenght;
         private System.Windows.Forms.TextBox TextBoxHeight;
         private System.Windows.Forms.TextBox TextBoxLenght;
-        private System.Windows.Forms.TextBox TextBoxWidht;
+        private System.Windows.Forms.TextBox TextBoxWidth;
         private System.Windows.Forms.TextBox TextBoxBladeLenght;
         private System.Windows.Forms.TextBox TextBoxInnerLenght;
         private System.Windows.Forms.Button ButtonSave;
         private System.Windows.Forms.Panel PanelWindow;
         private System.Windows.Forms.Label LabelParameter;
+        private System.Windows.Forms.ToolTip toolTipInformation;
+        private System.Windows.Forms.ToolStrip toolStrip1;
     }
 }
 
