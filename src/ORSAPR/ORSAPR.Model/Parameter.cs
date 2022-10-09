3f7e25d954e2981306a6a2435c16a6b4ed4e19cb
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2022-10-09 23:43:56 +0700

    class parameter completed

--- src/ORSAPR/ORSAPR.Model/Parameter.cs
+++ src/ORSAPR/ORSAPR.Model/Parameter.cs
@@ -10,6 +10,90 @@ namespace ORSAPR.Model
     /// класс параметров зубила
     /// </summary>
     public class Parameter
-    {   
+    {
+        /// <summary>
+        /// поле высоты зубила
+        /// </summary>
+        private double _height;
+
+        /// <summary>
+        /// поле длины зубила
+        /// </summary>
+        private double _lenght;
+
+        /// <summary>
+        /// поле ширины зубила
+        /// </summary>
+        private double _width;
+
+        /// <summary>
+        /// поле длины внутреннего выреза зубила
+        /// </summary>
+        private double _innerLenght;
+
+        /// <summary>
+        /// поле длины лезвия зубила
+        /// </summary>
+        private double _bladeLenght;
+
+        /// <summary>
+        /// возвращает или задает высоту
+        /// </summary>
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+            }
+        }
+
+        /// <summary>
+        /// возвращает или задает длину
+        /// </summary>
+        public double Lenght
+        {
+            get => _lenght;
+            set
+            {
+                _lenght = value;
+            }
+        }
+
+        /// <summary>
+        /// возвращает или задает ширину
+        /// </summary>
+        public double Width
+        {
+            get => _width;
+            set
+            {
+                _width = value;
+            }
+        }
+
+        /// <summary>
+        /// возвращает или задает длину внутреннего выреза зубила
+        /// </summary>
+        public double InnerLenght
+        {
+            get => _innerLenght;
+            set
+            {
+                _innerLenght = value;
+            }
+        }
+
+        /// <summary>
+        /// возвращает или задает длину лезвия зубила
+        /// </summary>
+        public double BladeLenght
+        {
+            get => _bladeLenght;
+            set
+            {
+                _bladeLenght = value;
+            }
+        }
     }
 }
